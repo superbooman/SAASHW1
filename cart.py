e4d5356e050c8f438ecbe18bf1a57b294456ee37
superbooman <90643769+superbooman@users.noreply.github.com>
2023-09-25 23:03:08 -0400

    Add files via upload

--- cart.py
+++ cart.py
@@ -10,26 +10,64 @@ def get_cart():
     return jsonify(cart)
 
 #Endpoint #2: Post quantity of product to cart (using id)
-@app.route('/cart', methods = ['POST'])
-def add_cart():
-    new_item = request.json.get('id')
-    product = next((product for product in product if product["id"] == product), None)
-    if product:
-        cart.append(product)
-        return jsonify({"product": product})
-    else:
-        return jsonify({"error": "Product not found"}), 404
+@app.route('/cart/<int:user_id>/add/<int:product_id>', methods = ['POST'])
+def add_cart(user_id,product_id):
     
-#Endpoint #3: Post removal of quantity from cart (using id)
-@app.route('/cart', methods = ['POST'])
-def add_cart():
-    new_item = request.json.get('id')
-    product = next((product for product in product if product["id"] == product), None)
-    if product:
-        cart.pop(product)
-        return jsonify({"product": product})
+
+    data = request.json
+    quantity = data.get('quantity')
+    quantityid = {"quantity":quantity, 
+                  "product_id":product_id}
+    if not quantity:
+        return jsonify({"error": "Quantity not found"}), 404
+    
+    response = request.post('http://127.0.0.1:5000/product_list/remove_quantity', json = quantityid)
+    
+    current_cart_id = len(cart)+1
+
+    new_cartproduct = {
+        "id": current_cart_id,
+        "name": response.json.get('name'),
+        "price": response.json.get('price'),
+        "quantity": quantity
+    }
+    cart.append(new_cartproduct)
+
+    cart_item = next((cart_item for cart_item in cart if cart_item["id"] == current_cart_id), None)
+    if cart_item:
+        return jsonify({"cart_item": cart_item}), 200
     else:
-        return jsonify({"error": "Product not found"}), 404
+        return jsonify({"error": "Cart Item unable to be added"}), 404
+    
+#Endpoint #3: Remove quantity from user cart
+@app.route('/cart/<int:user_id>/remove/<int:product_id>', methods = ['POST'])
+def add_cart(user_id,product_id):
+    
+
+    data = request.json
+    quantity = data.get('quantity')
+    quantityid = {"quantity":quantity, 
+                  "product_id":product_id}
+    if not quantity:
+        return jsonify({"error": "Quantity not found"}), 404
+    
+    response = request.post('http://127.0.0.1:5000/product_list/add_quantity', json = quantityid)
+    
+    current_cart_id = len(cart)+1
+
+    new_cartproduct = {
+        "id": current_cart_id,
+        "name": response.json.get('name'),
+        "price": response.json.get('price'),
+        "quantity": quantity
+    }
+    cart.pop(new_cartproduct)
+
+    cart_item = next((cart_item for cart_item in cart if cart_item["id"] == current_cart_id), None)
+    if cart_item:
+        return jsonify({"cart_item": cart_item}), 200
+    else:
+        return jsonify({"error": "Cart Item unable to be removed"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- product.py
+++ product.py
@@ -18,7 +18,7 @@ def get_products():
 #Endpoint 2: get individual product
 @app.route('/product_list/<int:product_id>', methods=['GET'])
 def get_product(product_id):
-    product = next((product for product in product if product["id"] == product_id), None)
+    product = next((product for product in product_list if product["id"] == product_id), None)
     if product:
         return jsonify({"product": product})
     else:
@@ -35,6 +35,35 @@ def create_product():
     }
     product_list.append(new_product)
     return jsonify({"message": "Product created", "product": new_product}), 201
+
+#Endpoint 4: modify quantity value down
+@app.route('/product_list/remove_quantity', methods=['POST'])
+def remove_quantity():
+    data = request.json
+    quantity_remove = data.get('quantity')
+    id = data.get('product_id')
+
+    product = next((p for p in product_list if p["id"] == id), None)
+    if product:
+        product['quantity'] = product['quantity'] - quantity_remove
+        return jsonify({"message": "Product quantity modified successfully", "product": product})
+    else:
+        return jsonify({"error": "Product not found"}), 404
+
+#Endpoint 5: modify quantity value up
+@app.route('/product_list/add_quantity', methods=['POST'])
+def remove_quantity():
+    data = request.json
+    quantity_add = data.get('quantity')
+    id = data.get('product_id')
+
+    product = next((p for p in product_list if p["id"] == id), None)
+    if product:
+        product['quantity'] = product['quantity'] + quantity_add
+        return jsonify({"message": "Product quantity modified successfully", "product": product})
+    else:
+        return jsonify({"error": "Product not found"}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
